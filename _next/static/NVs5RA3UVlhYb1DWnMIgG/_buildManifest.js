5cbbdc4ed89d52523825b15d3653ab7aa864d772
ViXx07 <vicen.perezb@duocuc.cl>
2026-02-26 22:24:06 -0300

    Updates

---
+++ _next/static/NVs5RA3UVlhYb1DWnMIgG/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/pagina-portafolio//_next/:path+",
+        "destination": "/pagina-portafolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
